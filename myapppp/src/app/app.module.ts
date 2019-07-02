b7ea66d55d3495690b5f3c968467d807a095e14a
Vaishali-web <vaishali.iprimed@gmail.com>
2019-07-02 16:02:09 +0530

    sfdsdf

--- myapppp/src/app/app.module.ts
+++ myapppp/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserItemComponent } from './user-item/user-item.component';
+import { HelloworldComponent } from './helloworld/helloworld.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    UserItemComponent,
+    HelloworldComponent
   ],
   imports: [
     BrowserModule,
